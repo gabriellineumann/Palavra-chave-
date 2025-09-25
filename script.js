f945b1632370be6cbb9e44c154c107bdf410afc0
gabriellineumann <neumann.gabrielli@escola.pr.gov.br>
2025-09-25 08:42:11 -0300

    palavras ruins

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { PALAVRAS_RUINS } from "./palavrasRuins2.js";
+import { PALAVRAS_RUINS } from "./palavrasruins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
